e6da2431dcfc9d49ecc8f4f89847d0660d7edcb9
MargaretLai <MargaretLUI01@gmail.com>
2024-06-10 13:44:44 -0400

    Added js to handle item purchase (to be improved later)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { menuArray } from './data.js'
 
 // Render food items
 const mainSection = document.getElementById("main")
+const totalSection = document.getElementById("total-section")
 
 const mainHTML = menuArray.map(item => {
     const {name, ingredients, id, price, emoji} = item
@@ -10,7 +11,7 @@ const mainHTML = menuArray.map(item => {
     <div class="item">
         <h1 class="item-emoji">${item.emoji}</h1>
         <h3 class="item-name">${item.name}</h3>
-        <i class="fa-solid fa-cart-plus"></i>
+        <i class="fa-solid fa-cart-plus" data-id="${item.id}"></i>
         <h5 class="item-ingredients">${item.ingredients}</h5>
         <h4 class="item-price">${item.price}</h4>
         <hr>
@@ -20,4 +21,32 @@ const mainHTML = menuArray.map(item => {
 
 mainSection.innerHTML = mainHTML
 
-// Calculation
+// Total
+document.addEventListener("click", function(e) {
+    if (e.target.dataset.id) {
+        renderTotalSection()
+    }
+})
+
+function renderTotalSection() {
+    totalSection.innerHTML =   `
+    <h3 id="title">Your order</h3>
+    <div class="order-detail">
+        <div class="food">
+            <h3 class="food-name">Pizza <button class="remove-btn">remove</button></h3>
+            <h3 class="food-price">$14</h3>
+        </div>
+    </div>
+
+    <div class="total">
+        <hr id="total-hr">
+
+        <div class="total-detail">
+            <h3 id="total-title">Total price: </h3>
+            <h3 id="total-price">$23</h3>
+        </div>
+
+        <button id="pay-btn">Complete order</button>
+    </div>
+    `
+}
